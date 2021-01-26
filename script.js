7d69637d2e03d85d1ae6a43c85dc0f2213ebf294
verona-hub <68257343+batman-project@users.noreply.github.com>
2021-01-26 18:41:12 +0100

    Fxed Edit button

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/*jshint esversion: 6 */
+/* jshint esversion: 6 */
 
 $(document).ready(function() {
 
@@ -21,14 +21,7 @@ $(document).ready(function() {
     if ($emptyInput.length !== 0) {
 
       // Append new task to the list
-      $('ol').append(
-        '<li>' +
-        '<span class="task"> ' + todoText + '</span>' +
-        '<span class="trash" contenteditable="false">   <i class="fas fa-trash fa-lg">      </i></span>' +
-        '<span class="edit" contenteditable="false">    <i class="fas fa-pencil-alt fa-lg"> </i></span>' +
-        '<span class="checked" contenteditable="false"> <i class="far fa-square fa-lg">     </i></span>' +
-        '</li>'
-      );
+      $('ol').append('<li>' + '<span class="task"> ' + todoText + '</span>' + '<span class="trash" contenteditable="false">   <i class="fas fa-trash fa-lg">      </i></span>' + '<span class="edit" contenteditable="false">    <i class="fas fa-pencil-alt fa-lg"> </i></span>' + '<span class="checked" contenteditable="false"> <i class="far fa-square fa-lg">     </i></span>' + '</li>');
       // Clear the input after new task is inserted
       $('input').val('');
 
@@ -50,7 +43,6 @@ $(document).ready(function() {
 
   // Clear the input if the page is refreshed
   $("input").val('');
-
 
   /*
   ██████  ██      ██    ██ ███████
@@ -86,7 +78,6 @@ $(document).ready(function() {
 
   });
 
-
   /*
    ██████  ██████  ███    ███ ██████  ██      ███████ ████████ ███████
   ██      ██    ██ ████  ████ ██   ██ ██      ██         ██    ██
@@ -106,7 +97,6 @@ $(document).ready(function() {
 
   });
 
-
   /*
   ███████ ██████  ██ ████████
   ██      ██   ██ ██    ██
@@ -115,14 +105,20 @@ $(document).ready(function() {
   ███████ ██████  ██    ██
   */
 
-  /// EDIT TASK ICON
+  // Function to Finish Editing the task
+  function finishEditTask() {
+    $(this).closest('li').prop('contenteditable', false);
+    $(this).closest('li').find('span.edit').removeClass('orange');
+  }
+
+  /// EDIT TASK
   $('ol').on('click', 'span.edit', function() {
 
     // Start editing the task
     if (!$(this).parent('li').hasClass('completed')) {
-      $(this).closest('li').find('.task').prop('contenteditable', true).focus().val();
+      $(this).closest('li').prop('contenteditable', true).focus().val();
 
-      // Pencil icon changes color on click
+      // Pencil icon toggles orange color
       if ($(this).hasClass('orange')) {
         $(this).removeClass('orange');
       } else {
@@ -130,40 +126,31 @@ $(document).ready(function() {
       }
     }
 
-    // Function to finish editing the task
-    function finishEditTask() {
-      $(this).closest('li').find('.task').prop('contenteditable', false);
-      $(this).closest('li').find('span.edit').removeClass('orange');
-    }
-
-    // If keyboard ENTER is pressed, the edit finishes
+    // If ENTER is pressed, the edit finishes
     $('ol').on('keypress', 'li', function(event) {
       if (event.keyCode === 13) {
-        $(this).closest('li').find('.task').prop('contenteditable', false);
+        $(this).closest('li').prop('contenteditable', false);
         $(this).closest('li').find('span.edit').removeClass('orange');
       }
     });
 
-    // If a click happens outside of the task list, the edit finishes
+    // If click goes outside of the task list, the edit finishes
     $('li').on('focusout', finishEditTask);
 
     // When edit button is clicked again, the edit finishes
-    $('li').on('click', '.edit', function() {
-
+    $('li').on('click', 'span.edit', function() {
       if ($(this).closest('li').prop('contenteditable') == 'true') {
-
-        $(this).closest('li').find('.task').prop('contenteditable', false);
+        $(this).closest('li').prop('contenteditable', false);
         $(this).closest('li').find('span.edit').removeClass('orange');
 
         return false;
       }
     });
 
-    // if the Complete checkbox icon is clicked, the edit finishes
+    // if the Complete checkbox is clicked, the edit function finishes
     $('li').on('click', 'span.checked', finishEditTask);
 
   });
-
 
   /*
   ██████  ███████ ██      ███████ ████████ ███████
@@ -187,7 +174,6 @@ $(document).ready(function() {
     // Prevent bubbling
     event.stopPropagation();
   });
-
 
   /// End of Document
 });
