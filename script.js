3782f24069b2b9eadce7b7351293314b9d4b6be8
Zoran G <68257343+verona-hub@users.noreply.github.com>
2021-06-08 20:18:49 +0100

    - minor changes to the script

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-/* jshint esversion: 6 */
-
 $(document).ready(function() {
 
   /// Main funtion to add a task to the list
-  const addTaskToList = () => {
+  const addTask = () => {
 
-    // Text from the input
+    // The input text is taken and trimmed to remove whitespace
     let todoText = $('input').val().trim();
-
     // Take the first task letter and make it Uppercase
     const firstLetter = todoText.slice(0, 1).toUpperCase();
     // Rest of the letters
     const restOfLetters = todoText.slice(1, todoText.length);
-    // The input task is now modified
+    // The input text is now modified
     todoText = firstLetter + restOfLetters;
 
     // Declare an empty input
@@ -22,7 +19,7 @@ $(document).ready(function() {
     // If input is not empty:
     if ($emptyInput.length !== 0) {
 
-      // Append new task to the list
+      // Append the new task to the list
       $('ol').append(`
         <li>
           <span class="task"> ${todoText} </span> 
@@ -30,20 +27,23 @@ $(document).ready(function() {
           <span class="edit"    contenteditable="false" title="Click to edit this task">                 <i class="fas fa-pencil-alt fa-lg"> </i></span>
           <span class="checked" contenteditable="false" title="Click to mark this task as completed">    <i class="far fa-square fa-lg">     </i></span>
         </li>`);
+
       // Clear the input after a new task is submitted
       $('input').val('');
     }
-  }
+    
+    // End of addTask function
+  };
 
-  // Add the task to the list when (+) button is pressed and when input is not empty
-  $('.wrapper').on('click', '#plus', addTaskToList);
+  // Add the task to the list when (+) button is pressed while input is not empty
+  $('.wrapper').on('click', '#plus', addTask);
 
-  // Add the task to the list when keyboard ENTER is pressed and when input is not empty
+  // Add the task to the list when keyboard ENTER is pressed while input is not empty
   const addToTaskListKeypress = event => {
-    event.keyCode === 13 && addTaskToList();
+    event.keyCode === 13 && addTask();
   }
 
-  // Add task to the list if the focus goes outside of the input and the keyboard ENTER is pressed
+  // Add the task to the list if the focus goes outside of the input and the keyboard ENTER is pressed
   $(document, 'input').on('keydown', addToTaskListKeypress);
 
   // Clear the input if the page is refreshed
@@ -69,7 +69,7 @@ $(document).ready(function() {
   // Hide the (+) icon as default
   $plusIcon.hide();
 
-  // Show the (+) icon when: input is pressed and is empty
+  // Show the (+) icon if: input is pressed and input is empty
   $('.wrapper').on('focus', 'input', () => {
     $plusIcon.show();
   });
@@ -87,14 +87,16 @@ $(document).ready(function() {
    ██████  ██████  ██      ██ ██      ███████ ███████    ██    ███████
   */
 
-  /// Complete task icon
-  $('ol').on('click', 'span.checked', () => {
+  /// Complete a task by clicking the square icon
+  $('ol').on('click', 'span.checked', completeTask);
+
+  // Complete function
+  function completeTask() {
     // The checkbox becomes ticked and green while the icon is replaced with a checked one
     $(this).closest('span').find('i').toggleClass('fa-square fa-check-square green');
     // The task is dimmed and a line goes through the text
     $(this).parent('li').toggleClass('completed');
-  });
-
+  }
 
   /*
   ███████ ██████  ██ ████████
@@ -104,20 +106,17 @@ $(document).ready(function() {
   ███████ ██████  ██    ██
   */
 
-  // Function to Finish Editing the task
-  const finishEditTask = () => {
-    $(this).closest('li').prop('contenteditable', false);
-    $(this).closest('li').find('span.edit').removeClass('orange');
-  }
+  /// Edit a task by clicking the pencil icon
+  $('ol').on('click', 'span.edit', editTask);
 
-  /// EDIT TASK
-  $('ol').on('click', 'span.edit', function() {
-
-    // Start editing the task
+  // Edit function
+  function editTask() {
+    // Start editing the task if it is not marked as completed
     if (!$(this).parent('li').hasClass('completed')) {
+      // The focus is moved to the task, which can now be edited
       $(this).closest('li').prop('contenteditable', true).focus().val();
 
-      // Pencil icon toggles orange color
+      // Pencil icon toggles orange color when edit is active/inactive
       if ($(this).hasClass('orange')) {
         $(this).removeClass('orange');
       } else {
@@ -125,19 +124,26 @@ $(document).ready(function() {
       }
     }
 
-    // If ENTER is pressed, the edit finishes
-    $('ol').on('keypress', 'li', function(event) {
+    // Function to finish editing the task
+    function stopEditTask() {
+      $(this).closest('li').prop('contenteditable', false);
+      $(this).closest('li').find('span.edit').removeClass('orange');
+    };
+
+    // If ENTER is pressed during edit, the edit stops
+    $('ol').on('keypress', 'li', event => {
       if (event.keyCode === 13) {
         $(this).closest('li').prop('contenteditable', false);
+        // As the edit finishes, the orange color is removed
         $(this).closest('li').find('span.edit').removeClass('orange');
       }
     });
 
-    // If click goes outside of the task list, the edit finishes
-    $('li').on('focusout', finishEditTask);
+    // If during edit a click happens outside the task list, the edit stops
+    $('li').on('focusout', stopEditTask);
 
-    // When edit button is clicked again, the edit finishes
-    $('li').on('click', 'span.edit', function() {
+    // Editception = when the edit button is clicked again during edit, the edit stops
+    $('li').on('click', 'span.edit', () => {
       if ($(this).closest('li').prop('contenteditable') == 'true') {
         $(this).closest('li').prop('contenteditable', false);
         $(this).closest('li').find('span.edit').removeClass('orange');
@@ -146,10 +152,11 @@ $(document).ready(function() {
       }
     });
 
-    // if the Complete checkbox is clicked, the edit function finishes
-    $('li').on('click', 'span.checked', finishEditTask);
+    // If the Complete square box is clicked during edit, the edit stops
+    $('li').on('click', 'span.checked', stopEditTask);
 
-  });
+    // End of editTask function
+  }
 
   /*
   ██████  ███████ ██      ███████ ████████ ███████
@@ -159,20 +166,19 @@ $(document).ready(function() {
   ██████  ███████ ███████ ███████    ██    ███████
   */
 
-  /// DELETE TASK ICON
-  $('ol').on('click', 'span.trash', function(event) {
+  /// Delete a task by clicking the recycle bin icon
+  $('ol').on('click', 'span.trash', deleteTask);
 
+  // Delete function
+  function deleteTask(event) {
     // Fadeout effect before removal
     $(this).parent().fadeTo(1000, 0.05).fadeOut(250, function() {
-
       // After the fadeout, the task is removed
       $(this).remove();
-
     });
-
     // Prevent bubbling
     event.stopPropagation();
-  });
+  }
 
   /// End of Document
 });
